73810c0f697e8288094d651b0766f4c46561f923
unknown <kaohhs@gmail.com>
2022-07-11 20:15:40 -0300

    se arregla conexión a BBDD

--- src/database.js
+++ src/database.js
@@ -1,21 +1,11 @@
-const mongoose = require('mongoose');
-const URI = process.env.URI;
-/* 
-mongoose.connect(URI)
-.then(db => console.log('La base de datos se conectó correctamente'))
-.catch(err => console.error(err));
- */
+const mongoose = require('mongoose')
 
-//asi le damos un pool de conexiones!
-const options = {
-    maxPoolSize : 10,
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-}
-
-mongoose.connect(URI,options,(err)=>{
-    err?console.log('No se puede conectar a MONGO (ATLAS)'):console.log('Conectado a MONGO (ATLAS)');
-
-});
-
-module.exports = mongoose;
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.vgbzmud.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Conectado a la base de datos')
+  })
+  .catch((e) => {
+    console.log('Database error', e)
+  })
