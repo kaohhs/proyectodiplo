38329c2e2b2298d9e7bddea794d2bd7206587c5b
krat0s <kaohhs@gmail.com>
2022-07-12 07:19:07 -0300

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   entry: './src/app/index.js',
   output: {
-    path: __dirname + './src/public/js',
+    path: __dirname + '../src/public/js',
     filename: 'bundle.js'
   },
   module: {
